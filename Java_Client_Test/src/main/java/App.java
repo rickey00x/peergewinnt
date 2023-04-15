0ecdaa23d45a7f5aba1d0ee591b193e05e528352
Norman <rickey00x@gmail.com>
2023-04-15 16:11:49 +0200

    Changes by User

--- Java_Client_Test/src/main/java/App.java
+++ Java_Client_Test/src/main/java/App.java
@@ -1,4 +1,5 @@
 import Gui.GameWindow.GameWindow;
+import Gui.StartWindow.SelectionWindow;
 import model.Game;
 import model.InvalidMoveException;
 import model.Token;
@@ -9,32 +10,12 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My Panel");
-        GameWindow mainWindow = new GameWindow();
-        frame.add(mainWindow);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Game game = new Game("p1","p2");
-        try {
-            game.move(3);
-            game.move(3);
-            game.move(2);
-            game.move(6);
-            game.move(3);
-            game.move(3);
-            game.move(2);
-            game.move(6);
-            game.move(3);
-            game.move(3);
-            game.move(2);
-            game.move(6);
-            game.move(34);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        mainWindow.gamePanel.matrix.updateMatrix(game.getPlayingField());
+        JFrame mainFrame = new JFrame("Peergewinnt");
+        SelectionWindow start = new SelectionWindow();
+        mainFrame.add(start);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
